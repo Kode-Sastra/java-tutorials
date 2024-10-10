eac4a24f7258452d22233e54ce0b99f835b85ac7
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2024-10-10 23:45:58 +0530

    Issue-1, updates on pom and basic structure of maven modules

---
+++ springboot-modules/spring-ai-qdrant/src/test/java/com/kodesastra/spring/qdrant/SpringQdrantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodesastra.spring.qdrant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQdrantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
