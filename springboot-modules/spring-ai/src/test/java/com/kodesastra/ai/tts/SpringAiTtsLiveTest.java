60b040d4ecc62c21641a9a825952ba3db196ebe4
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2025-05-11 23:42:49 +0530

    Issue-24, Spring AI TTS API

--- springboot-modules/spring-ai/src/test/java/com/kodesastra/ai/tts/SpringAiTtsLiveTest.java
+++ springboot-modules/spring-ai/src/test/java/com/kodesastra/ai/tts/SpringAiTtsLiveTest.java
@@ -1,9 +1,10 @@
 package com.kodesastra.ai.tts;
 
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
 import java.io.IOException;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
